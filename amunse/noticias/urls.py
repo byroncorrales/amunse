a4dfea83444f965833a0fd529b5d310f7228db8f
bcorrales <bcorrales@bcorrales-laptop.(none)>
2010-10-17 23:45:47 -0600

    Filtros de noticias por categorias y videos

--- amunse/noticias/urls.py
+++ amunse/noticias/urls.py
@@ -4,5 +4,6 @@ from models import Noticia
 
 urlpatterns = patterns('noticias.views',
     (r'^noticias/(?P<slug>[-\w]+)/$', 'noticia_detalle'),
+    (r'^noticias/categoria/(?P<cat_slug>[-\w]+)/$', 'noticia_lista_cat'),
     (r'^noticias/$', 'noticia_lista'),
 )
--- amunse/noticias/views.py
+++ amunse/noticias/views.py
@@ -5,17 +5,56 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from django.views.generic.simple import direct_to_template
 from django.core.exceptions import ViewDoesNotExist
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 from noticias.models import *
 
 def noticia_detalle(request,slug):
+    '''Muestra el detalle de la noticia'''
     noticia = get_object_or_404(Noticia, slug=slug)
+    #Jala las ultimas noticias relacionadas con la misma categoria y excluye a la noticia misma
+    noticia_rel = Noticia.objects.filter(categoria=noticia.categoria).exclude(id = noticia.id).order_by('-fecha','-id')[:3] 
     dicc = {'noticia': noticia,
+            'noticia_rel':noticia_rel,
            }
     return direct_to_template(request, 'noticias/noticia_detalle.html',dicc)
 
 def noticia_lista(request):
-    noticia = Noticia.objects.all().order_by('-fecha','-id')
-    dicc = {'noticia': noticia,
+    '''Vista para mostrar la lista de noticia'''
+    noticia_lista = Noticia.objects.all().order_by('-fecha','-id')
+    paginator = Paginator(noticia_lista, 5)
+    categoria_noticia = CategoriaNoticia.objects.all()
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        noticia = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        noticia = paginator.page(paginator.num_pages)
+
+    dicc = {'noticias': noticia,'categoria_noticia':categoria_noticia,
            }
     return direct_to_template(request, 'noticias/noticias_lista.html',dicc)
+    
+def noticia_lista_cat(request,cat_slug):
+    '''Filtra la lista de noticias por una categoria especifica'''
+    noticia_lista = Noticia.objects.filter(categoria__slug=cat_slug).order_by('-fecha','-id')
+    categoria = CategoriaNoticia.objects.get(slug=cat_slug)
+    paginator = Paginator(noticia_lista, 5)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        noticia = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        noticia = paginator.page(paginator.num_pages)
+
+    dicc = {'noticias': noticia,'categoria':categoria,
+           }
+    return direct_to_template(request, 'noticias/noticias_lista_cat.html',dicc)
