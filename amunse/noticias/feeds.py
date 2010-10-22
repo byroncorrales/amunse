9c2b0a97ce0c59c3273bba40a84ce95b47830ac8
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2010-10-22 13:51:11 -0600

    agregando feed a noticia

---
+++ amunse/noticias/feeds.py
@@ -0,0 +1,23 @@
+from django.contrib.syndication.views import Feed
+from models import Noticia
+
+
+class NoticiasFeed(Feed):
+    title = "Noticias de AMUNSE"
+    link = "/noticias/feed/"
+    description = 'Noticias mas recientes de AMUNSE'
+    
+    def items(self):
+        return Noticia.objects.order_by('-fecha')
+
+    def item_title(self, item):
+        return item.titulo
+
+    def item_description(self, item):
+        return item.contenido
+
+    def item_author_name(self, item):
+        return item.autor 
+
+    def item_link(self, item):
+        return item.get_full_url()
