3e1f03140b72eb3e77663f8c4f9d306688e91e08
Byron Corrales <byroncorrales@gmail.com>
2012-07-23 14:30:02 -0600

    fix formato id

--- amunse/documentos/urls.py
+++ amunse/documentos/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('documentos.views',
     (r'^documentos/$', 'categoria_lista'),
     (r'^documentos/(?P<slug_cat>[-\w]+)/$', 'subcategoria_lista'),
     (r'^documentos/(?P<slug_cat>[-\w]+)/(?P<slug_subcat>[-\w]+)/$', 'archivo_lista'),
-    (r'^documentos/(?P<slug_cat>[-\w]+)/(?P<slug_subcat>[-\w]+)/(?P<archivo_slug>[-\w]+)$', 'archivo_detalle'),
+    (r'^documentos/(?P<slug_cat>[-\w]+)/(?P<slug_subcat>[-\w]+)/(?P<archivo_slug>\d+)$', 'archivo_detalle'),
 )
